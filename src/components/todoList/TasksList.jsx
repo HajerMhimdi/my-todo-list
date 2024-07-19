bfc0e376015495a9a9b9361a44fcd25bae2c2b47
hajer <hajer.mhimdi@gmail.com>
2024-07-19 09:23:48 +0100

    adjusting empty tasks

--- src/components/todoList/TasksList.jsx
+++ src/components/todoList/TasksList.jsx
@@ -12,17 +12,15 @@ function TaskList({ tasks, onDelete, onComplete }) {
 
   return (
     <>
-
-        <section className="tasks">
-
-          <div className="list">
-            {tasks.map((task) => (
-              <Task key={task.id} task={task} onDelete={onDelete} onComplete={onComplete} />
-
-            ))}
-          </div>
-
-        </section>
+      <div className="list">
+      {tasks.length > 0 ? (
+        tasks.map((task) => (
+          <Task key={task.id} task={task} onDelete={onDelete} onComplete={onComplete} />
+        ))
+      ) : (
+        <p className='emptyTasks'>No tasks available</p>
+      )}
+    </div>
     </>
 
   )
